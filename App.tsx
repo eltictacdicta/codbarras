96fe072f9e2250bb8b6a7fc6435cbf45f7bbac9b
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2023-11-29 23:57:07 +0100

    primeras pruebas

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { View, TextInput, StyleSheet,Text } from 'react-native';
+import { Camera, useCodeScanner,useCameraPermission,useCameraDevice } from 'react-native-vision-camera';
+
+
+
+export default function App() {
+  const { hasPermission, requestPermission } = useCameraPermission()
+  const [codigoDeBarras, setCodigoDeBarras] = useState<string | null>(null);
+  const device = useCameraDevice('back')
+
+  if (device == null) return <Text > No tienes permisos </Text>
+  const codeScanner = useCodeScanner({
+    codeTypes: ['qr', 'ean-13'],
+    onCodeScanned: (codes) => {
+      console.log(`Scanned ${codes[0].value} codes!`)
+    }
+  })
+
+  return (
+    <View style={styles.container}>
+      <Camera 
+        style={StyleSheet.absoluteFill}
+        device={device}
+        isActive={true}     
+        codeScanner={codeScanner} />
+      <TextInput
+        onChangeText={text => setCodigoDeBarras(text)}
+        value={codigoDeBarras || ''}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+});
---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: [
+    ['react-native-worklets-core/plugin'],
+  ],
+  presets: ['module:metro-react-native-babel-preset'],
+};
