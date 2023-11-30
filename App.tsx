0a96773c194dec3ad47443122865981ffb90aebd
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2023-11-30 10:35:58 +0100

    añado comprobación de permisos de cámara

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, TextInput, StyleSheet,Text } from 'react-native';
 import { Camera, useCodeScanner,useCameraPermission,useCameraDevice } from 'react-native-vision-camera';
-
-
 
 export default function App() {
   const { hasPermission, requestPermission } = useCameraPermission()
   const [codigoDeBarras, setCodigoDeBarras] = useState<string | null>(null);
   const device = useCameraDevice('back')
 
+  useEffect(() => {
+    if (!hasPermission) {
+      requestPermission();
+    }
+  }, [hasPermission, requestPermission]);
+
+  if (!hasPermission) return <Text > No tienes permisos </Text>
   if (device == null) return <Text > No tienes permisos </Text>
+  
   const codeScanner = useCodeScanner({
     codeTypes: ['qr', 'ean-13'],
     onCodeScanned: (codes) => {
@@ -25,7 +31,7 @@ export default function App() {
         isActive={true}     
         codeScanner={codeScanner} />
       <TextInput
-        onChangeText={text => setCodigoDeBarras(text)}
+        //onChangeText={text => setCodigoDeBarras(text)}
         value={codigoDeBarras || ''}
       />
     </View>
